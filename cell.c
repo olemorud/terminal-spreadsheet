999cb1672baa9bbc17c1a164e346068b65dc8243
Ole Morud <olekr99@hotmail.com>
2022-12-25 23:33:44 +0100

    style changes

--- cell.c
+++ cell.c
@@ -1,12 +1,11 @@
 #include "cell.h"
 
 void
-init_cell(struct cell *c, size_t x_pos, size_t y_pos, char *text,
-          enum cell_type type)
+init_cell(struct cell *c, size_t x, size_t y, char *text, enum cell_type type)
 {
     c->referenced_by = NULL;
-    c->x_pos = x_pos;
-    c->y_pos = y_pos;
+    c->x_pos = x;
+    c->y_pos = y;
     c->type = type;
     c->text = strdup("");
     c->value.integer = 0;
