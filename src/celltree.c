b4da1e0d6e414b45b9f81dafb454ec361d628573
Ole Morud <olekr99@hotmail.com>
2022-12-26 03:25:11 +0100

    add binary search tree features

--- src/celltree.c
+++ src/celltree.c
@@ -13,3 +13,52 @@ delete_cell_tree(struct cell_tree_node *root)
 
     free(root);
 }
+
+/* returns true if a 'greater than' b */
+bool
+greater_than(int x_a, int y_a, int x_b, int y_b)
+{
+    if (x_a == x_b) {
+        return y_a > y_b;
+    }
+
+    return x_a > x_b;
+}
+
+struct cell*
+search_tree(struct cell_tree_node *root, int x, int y) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    struct cell *c = root->cell;
+
+    if (c->x_pos == x && c->y_pos == y) {
+        return root->cell;
+    }
+
+    if(greater_than(x, y, c->x_pos, c->y_pos)) {
+        return search_tree(root->right, x, y);
+    } else {
+        return search_tree(root->left, x, y);
+    }
+}
+
+void
+insert_node(struct cell_tree_node **root, struct cell *c)
+{
+    if (*root == NULL) {
+        *root = malloc(sizeof(struct cell_tree_node));
+        (*root)->cell = c;
+        (*root)->right->left = NULL;
+        (*root)->right->right = NULL;
+        return;
+    }
+
+    if (greater_than(c->x_pos, c->y_pos, (*root)->cell->x_pos, (*root)->cell->y_pos)) {
+        insert_node(&(*root)->right, c);
+    } else {
+        insert_node(&(*root)->left, c);
+    }
+}
+
--- src/celltree.h
+++ src/celltree.h
@@ -2,12 +2,15 @@
 #define CELLTREE_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include "cell.h"
 
+/* TODO: implement red-black tree */
 struct cell_tree_node {
     struct cell           *cell;
     struct cell_tree_node *right;
     struct cell_tree_node *left;
+    bool                   isRed;
 };
 
 void delete_cell_tree(struct cell_tree_node *root);
